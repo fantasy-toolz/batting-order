cd3e261237c04e5449c9bbfc68a75e98d47780a4
michael-petersen <petersen.michael.s@gmail.com>
2025-04-21 13:53:46 +0100

    reorganise; upgrade summarise-year tech

--- analysis/summarise-year.py
+++ analysis/summarise-year.py
@@ -18,10 +18,23 @@ def rearrange_name(player_name):
             return player_name.split(' ')[1]+' '+player_name.split(' ')[2]+', '+player_name.split(' ')[0]
 
 
+timetag = ''
 year = '2025'
 #outdir = 'Preseason/'
 #outdir = 'Postseason/'
 outdir = ''
+
+# default
+start_date = '2025-01-01'
+end_date = '2025-12-31'
+
+timelimit=True
+
+if timelimit:
+    timetag = '-limitedtime'
+    start_date = '2025-04-07'
+    end_date = '2025-4-21'
+
 
 teams = ['LAA', 'HOU', 'OAK', 'TOR', 'ATL', 'MIL', 'STL','CHC', 'AZ', 'LAD', 'SF', 'CLE', 'SEA', 'MIA','NYM', 'WSH', 'BAL', 'SD', 'PHI', 'PIT', 'TEX','TB', 'BOS', 'CIN', 'COL', 'KC', 'DET', 'MIN','CWS', 'NYY']
 
@@ -36,11 +49,16 @@ TeamTotal = dict()
 PlrTeam = dict()
 
 for team in teams:
-    D = np.genfromtxt('data/{}{}/{}.csv'.format(outdir,year,team),delimiter=',',dtype='S26',skip_header=1)
-    print('team: ',team,' games played: ',len(D[:,0]))
-    TeamTotal[team] = len(D[:,0])
-    #print('date,lineup1,lineup2,lineup3,lineup4,lineup5,lineup6,lineup7,lineup8,lineup9,',file=f)
-    ngames = len(D[:,0])
+    D = pd.read_csv('data/{}{}/{}.csv'.format(outdir,year,team))
+    D['date'] = D['date'].str.replace('a', '').str.replace('b', '') # remove doubleheader tags for analysis
+    D['date'] = pd.to_datetime(D['date'])
+
+    D = D[(D['date'] >= start_date) & (D['date'] <= end_date)] # Filter rows based on the date range
+
+    print('team: ',team,' games played: ',len(D['date']))
+    TeamTotal[team] = len(D['date'])
+
+    ngames = len(D['date'])
     for n in range(0,ngames):
         for i in range(0,10):
             if i==0:
@@ -49,11 +67,11 @@ for team in teams:
             else:
                 
                 try:
-                    PositionTotal[D[n][i].decode().lstrip()][i-1] += 1
+                    PositionTotal[D['lineup{}'.format(i)].values[n].lstrip()][i-1] += 1
                 except:
-                    PositionTotal[D[n][i].decode().lstrip()] = np.zeros(9)
-                    PositionTotal[D[n][i].decode().lstrip()][i-1] += 1
-                    PlrTeam[D[n][i].decode().lstrip()] = team
+                    PositionTotal[D['lineup{}'.format(i)].values[n].lstrip()] = np.zeros(9)
+                    PositionTotal[D['lineup{}'.format(i)].values[n].lstrip()][i-1] += 1
+                    PlrTeam[D['lineup{}'.format(i)].values[n].lstrip()] = team
                 
 
 
@@ -63,10 +81,10 @@ for team in teams:
 plrs = np.array(list(PositionTotal.keys()))
 nplrs = len(plrs)
 print("Players who have started a game this year:",nplrs)
-f = open('data/{}/Aggregate/Summaries/'.format(outdir)+year+'player-batting-order.csv','w')
+f = open('data/{}/Aggregate/Summaries/'.format(outdir)+year+'player-batting-order{}.csv'.format(timetag),'w')
 print('player,team,b1,b2,b3,b4,b5,b6,b7,b8,b9',file=f)
 
-g = open('data/{}Aggregate/Summaries/'.format(outdir)+year+'mean-player-batting-order.csv','w') 
+g = open('data/{}Aggregate/Summaries/'.format(outdir)+year+'mean-player-batting-order{}.csv'.format(timetag),'w') 
 print('player,avg,ngames,teamgames,team',file=g)
 for iplr,plr in enumerate(plrs):
     avgspot = np.nansum(np.arange(1,10,1)*PositionTotal[plr]/np.nansum(PositionTotal[plr]))
